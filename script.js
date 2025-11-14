b60f10d7e11bf9a52c40d684b9090268fa70d14b
shivalilapare00 <90516920+shivalilapare00@users.noreply.github.com>
2025-11-14 17:58:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	  if (s2.length === 0) return 0;
+	
+
+    const lowerS1 = s1.toLowerCase();
+    const lowerS2 = s2.toLowerCase();
+
+    return lowerS1.indexOf(lowerS2);
 }
 
 // Please do not change the code below
